902fd7a76a307cc188940acfffe8eadd6df38756
JYW <...>
2025-05-23 17:47:46 +0900

    feat: 지도 기능 구현

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,8 @@ DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 # 금감원 API 키
 FSS_API_KEY = env('FSS_API_KEY') 
+KAKAO_REST_API_KEY = env('KAKAO_REST_API_KEY')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,4 @@
 // src/constants.js
 export const API_BASE_URL = 'http://localhost:8000'
+
+export const KAKAO_API_KEY = import.meta.env.VITE_KAKAO_API_KEY
